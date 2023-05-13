d79369c4f7401c790731fb4acaf7bf1b39a28363
Anton Naumov <anton.naumov@inbox.ru>
2023-05-13 19:22:54 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
+import json
 
 
 bot = Bot('5742777204:AAH8RflW8JFx43DFqK54Spl2xx3JBe5_ViI')
@@ -11,6 +12,12 @@ async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
     markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://antoshkanaumov.github.io/bot_project/')))
     await message.answer('Привет мой друг!', reply_markup=markup)
+
+
+@dp.message_handler(content_types=['web_app_data'])
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f'Name: {res["name"]}. Email: {res["email"]}. Phone: {res["phone"]}')
 
 
 executor.start_polling(dp)
